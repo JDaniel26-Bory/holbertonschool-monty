66e7618d3be6d6fcce694defeca08b7b6690b976
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-05-11 13:51:57 -0500

    add the main.c for the fix

--- main.c
+++ main.c
@@ -3,43 +3,45 @@
 int error = 0;
 
 /**
- * main - monty program
+ * main - main monty program to handle the bytecode files
  * @argc: argument count
  * @argv: array of argument
- * Return: error
-*/
+ * Return: EXIT_SUCCESS for success, EXIT_FAILURE for failure
+ */
 
 int main(int argc, char **argv)
 {
-FILE *f;
-stack_t *stack = NULL;
-unsigned int line_number = 0;
-char str[1024];
-char *t = NULL;
-size_t n = 1024;
+	FILE *fd;
+	stack_t *stack = NULL;
+	unsigned int line_number = 0;
+	char str[1024];
+	char *tok = NULL;
+	size_t n = 1024;
 
-if (argc != 2)
-{
-fprintf(stderr, "USAGE: monty file\n");
-exit(EXIT_FAILURE);
-}
-f = fopen(argv[1], "r");
-if (f == NULL)
-{
-fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-exit(EXIT_FAILURE);
-}
-while (fgets(str, n, f) != NULL && error != 1)
-{
-line_number++;
-t = strtok(str, "\n\t");
-if (t != NULL)
-{
-check_opcode(t, &stack, line_number);
-}
-}
-free_all(stack, f);
-if (error == 1)
-exit(EXIT_FAILURE);
-return (EXIT_SUCCESS);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(str, n, fd) != NULL && error != 1)
+	{
+		line_number++;
+		tok = strtok(str, "\n\t ");
+		if (tok != NULL)
+		{
+			check_opcode(tok, &stack, line_number);
+		}
+	}
+	free_all(stack, fd);
+
+	if (error == 1)
+		exit(EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
