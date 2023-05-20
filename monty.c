d0cfbf1ead4855786df8f43370973571f3c3d84c
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-05-19 21:04:36 -0500

    fix the erros

--- monty.c
+++ monty.c
@@ -12,73 +12,100 @@ char *line = NULL;
 int main(int argc, char **argv)
 {
 
-    unsigned int line_number = 1;
-    unsigned int var;
-    char *command = NULL;
-    
-    stack_t *stack = NULL;
+	unsigned int line_number = 1;
+	char *command = NULL;
+	int var;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	stack_t *stack = NULL;
 
-    while((line = lines_reader(argv[1], line_number)) != NULL)
-    {
-        /* printf("%s\n", line); */
-        
-        command = strtok(line, " \r\t\n");
-        if (command == NULL)
-        {
-            line_number++;
-            free(line);
-            continue;
-        }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-        if (strcmp(command, "push") == 0)
-        {
-            var = 1;
-        }
-        else if (strcmp(command, "pall") == 0)
-        {
-            var = 2;
-        }
-        else 
-        {
-            var = 0;
-        }
+	while ((line = lines_reader(argv[1], line_number)) != NULL)
+	{
+		
+		command = strtok(line, " \r\t\n");
+		if (command == NULL)
+		{
+			line_number++;
+			free(line);
+			continue;
+		}
+		
+		if (strcmp(command, "push") == 0)
+		{
+			var = 1;
+		}
+		else if (strcmp(command, "pall") == 0)
+		{
+			var = 2;
+		}
+		else if (strcmp(command, "pint") == 0)
+		{
+			var = 3;
+		}
+		
+		else if (strcmp(command, "pop") == 0)
+                {
+                        var = 4;
+                }
+		else if (strcmp(command, "swap") == 0)
+                {
+                        var = 5;
+                }
+		else if (strcmp(command, "add") == 0)
+                {
+                        var = 6;
+                }
+		else if (strcmp(command, "nop") == 0)
+                {
+                        var = 7;
+                }
 
+		else
+		{
+			var = 0;
+		}
 
-    switch(var)
-    {
-        case 1:
-            push(&stack, line_number);
-            break;
+		switch (var)
+		{
+		case 1:
+			push(&stack, line_number);
+			break;
 
-        case 2:
-            pall(&stack, line_number);
-            break;
+		case 2:
+			pall(&stack, line_number);
+			break;
 
-        // operator doesn't match any case constant +, -, *, /
-        default:
-            dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, command);
-            exit(EXIT_FAILURE);
-    }
-        /*
+		case 3:
+			pint(&stack, line_number);
+			break;
 
-        if (strcmp(command, "nop") == 0)
-        {
-            nop(&stack, line_number);
-        }
+		case 4:
+                        pop(&stack, line_number);
+                        break;
+		case 5:
+                        swap(&stack, line_number);
+                        break;
+		case 6:
+                        add(&stack, line_number);
+                        break;
+		case 7:
+                        nop(&stack, line_number);
+                        break;	
+		default:
+			dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, command);
+			exit(EXIT_FAILURE);
+		}
 
-        */
-        
-        free(line);
-        line_number++;
-    }
+		free(line);
+		line_number++;
+	}
 
-    stack_freer(stack);
+	stack_freer(stack);
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
